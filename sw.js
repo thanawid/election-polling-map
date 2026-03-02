38e0834f5bd137fa75e4e45fc09a500835c80f73
Bangraknoi <78256158+thanawid@users.noreply.github.com>
2026-03-02 13:31:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple cache for offline shell (map tiles still need internet)
-const CACHE = "election-pins-2026-03-02";
+const CACHE = "election-pins-v14";
 const ASSETS = ["./","./index.html","./style.css","./app.js","./data.json","./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
